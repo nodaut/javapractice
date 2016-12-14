29a57c5b966b00da9a8f688947187baf00c3c879
nodaut <full_set@naver.com>
2016-12-14 15:04:18 +0900

    added JSONParser example

--- javapractice/src/javapractice/Jsontest.java
+++ javapractice/src/javapractice/Jsontest.java
@@ -4,7 +4,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
 
-
 public class Jsontest {
 
 	public static void main(String[] args) {
@@ -20,8 +19,36 @@ public class Jsontest {
 	jarr.add(jobj);
 	System.out.println(jarr);
 	
+	//JSONArray의 첫번째 요소를 꺼낸 후 JSONObject로 형변환 한 후 get 메서드를 이용해 key-value쌍 중 key가 "name"인 것의 value를 가져온다. 
+	System.out.println(((JSONObject)jarr.get(0)).get("name"));
+	
+	
+	JSONParser parser = new JSONParser();
+	String sarr = "[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]";
+	String sobj = "{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}";
+	/*
+	-----------------------------------------------------------------------------------------
+	[string을 JSONArray로 만들기]
+		string을 바로(또는 string을  Object형으로 casting한 후) JSONArray로 casting할 수 없다. 
+			->string을 올바로 JSONArray형으로 casting하려면 JSONParser의 parse메서드에 통과시켜야 한다.
+	-----------------------------------------------------------------------------------------
+	*/
+	
+	//jarr=(JSONArray)(Object)sarr; //=>에러
+	try{
+		//JSONArray형 string을 parser.parse을 이용해 파싱가능한 object로 만들고 JSONArray로 형변환
+		jarr=(JSONArray)(parser.parse(sarr));
+		
+		//JSONObject형 string을 parser.parse을 이용해 파싱가능한 object로 만들고 JSONObject로 형변환
+		jobj=(JSONObject)(parser.parse(sobj));
+	}catch(ParseException pe){
+		System.out.println("position: " + pe.getPosition());
 	}
 
+	System.out.println(jarr.get(1));
+	System.out.println(jobj.get("1"));
+	System.out.println(((JSONObject)jobj.get("1")).get("2"));
+	}
 }
 
 /*
