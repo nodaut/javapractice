355c61fbd336d2688f9a03f1f0ba8d5cb9270eef
nodaut <full_set@naver.com>
2017-01-19 17:51:33 +0900

    active mq test

---
+++ javapractice/src/activeMQTest/ActiveMQTest.java
@@ -0,0 +1,10 @@
+package activeMQTest;
+
+public class ActiveMQTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("active mq Test");
+	}
+
+}
---
+++ javapractice/src/javapractice/tmpTest.java
@@ -0,0 +1,17 @@
+package javapractice;
+
+public class tmpTest {
+
+	public static void main(String[] args) {
+		String test = "1484636223267|select sum(TX_BYTES), sum(RX_BYTES) from system_tp";
+		String[] parsed = test.split("\\|");
+		for(int i=0; i<parsed.length; i++)
+			System.out.println(parsed[i]);
+		
+		if(test.isEmpty())
+			System.out.println("empty");
+		else
+			System.out.println("not empty");
+	}
+
+}
