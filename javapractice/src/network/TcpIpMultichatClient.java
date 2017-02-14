1891f7cc75323c9543764c37bf4e612aa8d3373a
nodaut <full_set@naver.com>
2017-02-14 18:16:10 +0900

    multi chat client

--- javapractice/src/network/TcpIpMultichatClient.java
+++ javapractice/src/network/TcpIpMultichatClient.java
@@ -16,24 +16,61 @@ public class TcpIpMultichatClient {
 			try{
 				out=new DataOutputStream(socket.getOutputStream());
 				this.name = name;
-			}catch(Exception e){}
+			}catch(IOException e){}
 		}
 		
 		public void run(){
 			Scanner scanner = new Scanner(System.in);
 			try{
-				if(out!=null) out.writeUTF(name);
-				while(out!=null) out.writeUTF("["+name+"]"+scanner.nextLine());	//while(out!=null)이 false가 되면 DataOutputStream이 끊어진 것
-			}catch(Exception e){}
+				if(out!=null)
+					out.writeUTF(name);
+				//DataOutStream객체가 없어지기 전까지(접속이 끊기기 전까지) 계속 루프를 돌면서 client 사용자가 타이핑 한것을 DataOutStream 객체에 write함
+				while(out!=null)
+					out.writeUTF("["+name+"]"+scanner.nextLine());	//while(out!=null)이 false가 되면 DataOutputStream이 끊어진 것
+			}catch(IOException e){}
 		}
 	}
 	
 	static class ClientReceiver extends Thread{
+		Socket socket;
+		DataInputStream in;
 		
+		ClientReceiver(Socket socket){
+			this.socket=socket;
+			try {
+				in = new DataInputStream(socket.getInputStream());
+			} catch (IOException e) {}
+		}
+		
+		public void run(){
+			//DataInputStream객체가 없어지기 전까지(접속이 끊기기 전까지) 계속 루프를 돌면서 DataOutStream 객체로 들어오는 데이터를 read함
+			while(in!=null){
+				try {
+					System.out.println(in.readUTF());
+				} catch (IOException e) {}
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
+		String serverIP="127.0.0.1";
+		
+		if(args.length!=1){
+			System.out.println("USAGE : java TcpIpMultichatClient name\n exit..");
+			System.exit(0);
+		}
+		
+		try {
+			//소켓을 생성해 ClientSender 및 ClientReceiver쓰레드를 시작함
+			Socket socket = new Socket(serverIP,7777);
+			new ClientSender(socket,args[0]).start();
+			new ClientReceiver(socket).start();
 
+		} catch (ConnectException ce) {
+			ce.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
